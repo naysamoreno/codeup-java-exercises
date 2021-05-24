b8b0397c24c897ca36cc2a7848fc95115107de8e
Naysa Moreno <naysaymoreno@gmail.com>
2021-05-24 10:15:54 -0500

    hello world

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        int myFavoriteNumber = 18;
         String jeff = "Jeff";
-        System.out.print("Hello, World!" + jeff );
+        System.out.print("Hello, World!\n"  + jeff + myFavoriteNumber );
+        System.out.println(" Not a new line");
     }
 }
