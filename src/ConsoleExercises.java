2f575d389f6cf6f6687b1081170e56e4fe66cd93
Naysa Moreno <naysaymoreno@gmail.com>
2021-05-24 16:39:57 -0500

    first prompt

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
         System.out.format("The value of pi is approximately %2f", pi);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please insert an integer");
+        int userInput = scanner.nextInt();
+        System.out.println("The number you entered is " +  userInput);
     }
 
 }
