7f1edb592fb61cff8e7b7b6d34586ecf86f93fb2
Naysa Moreno <naysaymoreno@gmail.com>
2021-05-24 17:07:38 -0500

    three words prompt

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -5,9 +5,16 @@ public class ConsoleExercises {
         double pi = 3.14159;
         System.out.format("The value of pi is approximately %2f", pi);
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please insert an integer");
+        System.out.println("\n Please insert an integer ");
         int userInput = scanner.nextInt();
         System.out.println("The number you entered is " +  userInput);
+        System.out.println("\n please enter three words");
+        String firstWord = scanner.next();
+        String secondWord = scanner.next();
+        String thirdWord = scanner.next();
+        System.out.println("you said the following three words... \n first: " + firstWord + "\n second: " + secondWord + "\n third: " + thirdWord );
+
+
     }
 
 }
