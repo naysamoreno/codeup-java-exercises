87fb6df8f436dfb51617dbd8ea5a922d49a7e03e
Naysa Moreno <naysaymoreno@gmail.com>
2021-05-25 14:52:55 -0500

    part one of two

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,13 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.println(i++);
-        }
-//        do {
-//            i = 0
-//        } while (i < 100);
+//        long i = 5;
+//        while (i <= 15) {
+//            System.out.println(i++);
+//        }
+//        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+        long i= 0;
+        do {
+            System.out.println(i+=2);
+        } while (i > 100);
     }
 }
---
+++ src/ControlFlowLec.java
@@ -0,0 +1,8 @@
+public class ControlFlowLec {
+    public static void main(String[] args) {
+        int x = 5;
+        if (x == 5) {
+            System.out.println("it is equal");
+        }
+    }
+}
