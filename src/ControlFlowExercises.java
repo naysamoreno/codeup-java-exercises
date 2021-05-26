83f99a5a3bd726b847a0730fa17c7cb949a07585
Naysa Moreno <naysaymoreno@gmail.com>
2021-05-25 19:47:56 -0500

    table of powers

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -14,14 +14,14 @@ public class ControlFlowExercises {
 //
 //        } while (i < 1000000);
 
-        for (int x =100; x >= -10; x--) {
-            System.out.println(x);
-            x-=5;
-        }
-        for (long i =2; i < 1000000; i++) {
-            System.out.println(i);
-            i*=i;
-        }
+//        for (int x =100; x >= -10; x--) {
+//            System.out.println(x);
+//            x-=5;
+//        }
+//        for (long i =2; i < 1000000; i++) {
+//            System.out.println(i);
+//            i*=i;
+//        }
             /*You have to design the code such that the user has only three tries to guess the correct pin of the account. You set the pin as a constant with a final attribute. When correct display "Correct, welcome back." When incorrect display "Incorrect, try again.". When user runs out of tries display "Sorry but you have been locked out."
 program Starts:
 Please enter pin:
@@ -38,22 +38,40 @@ Output:
 Correct, welcome back.
  */
         Scanner scanner = new Scanner(System.in);
-        int pinNumber = 12345;
+//        int pinNumber = 12345;
+//
+//        for (int p =1; p <=3; p++) {
+//            System.out.println("please enter your access pin");
+//            int attempt = scanner.nextInt();
+//            if (attempt != pinNumber) {
+//                System.out.println("Incorrect, try again");
+//            } else if (attempt == pinNumber) {
+//                System.out.println("Correct, Welcome Back");
+//                break;
+//            } else if (p == 3 && attempt != pinNumber) {
+//                System.out.println("Sorry but you have been locked out");
+//            }
+//        }
+        //#3
+        int i = 0;
+        int userInput2 = 0;
+        do {
+            System.out.println("Please enter an integer");
+            int userInput = scanner.nextInt();
+            System.out.println("");
+            System.out.println("Here is your table!");
+            System.out.println("number | squared | cubed");
+            System.out.println("------ | ------- | -----");
 
-        for (int p =1; p <=3; p++) {
-            System.out.println("please enter your access pin");
-            int attempt = scanner.nextInt();
-            if (attempt != pinNumber) {
-                System.out.println("Incorrect, try again");
-            } else if (attempt == pinNumber) {
-                System.out.println("Correct, Welcome Back");
-                break;
-            } else if (p == 3 && attempt != pinNumber) {
-                System.out.println("Sorry but you have been locked out");
-            }
+            for (i = 1; i <= userInput; i++)
+                System.out.println("" + i + "\t  |\t" + Math.pow(i, 2) + "\t |\t" + (Math.pow(i, 3)));
+            System.out.println("would you like to conitinue? 0 for no // 1 for yes ");
+            userInput2 = scanner.nextInt();
+            if (userInput2 == 1) {
 
-        }
+            };
+        } while (userInput2 != 0);
+        System.out.println("thank you for playing");
     }
-
 
 }
