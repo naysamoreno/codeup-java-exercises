47012c12616a7e4c8fe0e25963100845831afaa7
Naysa Moreno <naysaymoreno@gmail.com>
2021-05-27 11:37:00 -0500

    methods

---
+++ src/MethodsExercises.java
@@ -0,0 +1,22 @@
+public class MethodsExercises {
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+    public static int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+    public static int divide(int num1, int num2) {
+        return num1/num2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(1, 2));
+        System.out.println(subtract(5, 4));
+        System.out.println(multiply(9, 50));
+        System.out.println(divide(50, 2));
+
+    }
+}
