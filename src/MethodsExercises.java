9c0a91ba5ef8ffc09dfa05cf22b9a22ad60ce214
Naysa Moreno <naysaymoreno@gmail.com>
2021-05-28 23:59:16 -0500

    update

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -11,12 +11,16 @@ public class MethodsExercises {
     public static int divide(int num1, int num2) {
         return num1/num2;
     }
+    public static int modulo(int num1, int num2) {
+        return num1%num2;
+    }
 
     public static void main(String[] args) {
         System.out.println(add(1, 2));
         System.out.println(subtract(5, 4));
         System.out.println(multiply(9, 50));
         System.out.println(divide(50, 2));
+        System.out.println(modulo(9,3));
 
     }
 }
