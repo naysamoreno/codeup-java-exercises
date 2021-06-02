9d8b0c2d6d873817368292e962a41f9804e27d7a
Naysa Moreno <naysaymoreno@gmail.com>
2021-06-02 09:47:11 -0500

    person completed

--- src/Person.java
+++ src/Person.java
@@ -24,6 +24,15 @@ public class Person {
     public static void main(String[] args) {
         Person testPerson = new Person("Naysa");
         System.out.println(testPerson.getName());
+        Person person1 = new Person("John");
+        Person person2 = new Person("John");
+        System.out.println(person1.getName().equals(person2.getName()));
+        System.out.println(person1 == person2);
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
     }
 }
 
