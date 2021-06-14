17ab838f3912cc5339cfc175b7fdb7189834fc77
Naysa Moreno <naysaymoreno@gmail.com>
2021-06-14 08:58:08 -0500

    grocery app update

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,82 +1,123 @@
 package util;
+
 import java.util.Scanner;
 
 public class Input {
     private Scanner scanner;
+//            = new Scanner(System.in);
 
     public Input() {
         this.scanner = new Scanner(System.in);
     }
 
     public String getString() {
-        System.out.println("give me a string");
+        return this.scanner.nextLine();
+    }
+
+    // TODO: **BONUS**
+    public String getString(String prompt) {
+        System.out.println(prompt);
         return this.scanner.nextLine();
     }
 
     public boolean yesNo() {
         String userInput = this.scanner.nextLine();
-        if (userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes")) {
-            return true;
-        } else {
-            return false;
+        return userInput.trim().equalsIgnoreCase("y") || userInput.trim().equalsIgnoreCase("yes");
+    }
+
+    // TODO: **BONUS**
+    public boolean yesNo(String prompt) {
+        System.out.println(prompt);
+        String userInput = this.scanner.next();
+        return userInput.trim().equalsIgnoreCase("y") || userInput.trim().equalsIgnoreCase("yes");
+    }
+
+    public int getInt() {
+        return this.scanner.nextInt();
+    }
+
+    // TODO: **BONUS**
+//    public int getInt(String prompt) {
+//        System.out.println(prompt);
+//        return this.scanner.nextInt();
+//    }
+
+    // refactor getInt method to use try-catch
+    public int getInt(String prompt){
+        int number;
+        try {
+            number = Integer.valueOf(getString(prompt));
+            return number;
+        } catch (NumberFormatException nfe){
+            System.out.println("Wrong input, try again: ");
+            return getInt(prompt);
         }
     }
 
     public int getInt(int min, int max) {
-        System.out.printf("Enter a number between %d and %d%n", min, max);
-        int userInput = scanner.nextInt();
-        if (userInput >= min && userInput <= max) {
-            return userInput;
+        System.out.printf("Give me a whole number between %d and %d", min, max);
+        int userNumber = this.scanner.nextInt();
+
+        if(userNumber >= min && userNumber <= max) {
+            return userNumber;
         } else {
-            System.out.println("that number is invalid");
+            System.out.println("That number is invalid.");
             return getInt(min, max);
         }
     }
 
-    public int getInt(int num) {
-        return num;
+    // TODO: **BONUS**
+    public int getInt(String prompt, int min, int max) {
+        System.out.println(prompt);
+        int userNumber = this.scanner.nextInt();
 
+        if(userNumber >= min && userNumber <= max) {
+            return userNumber;
+        } else {
+            System.out.println("That number is invalid.");
+            return getInt(min, max);
+        }
+    }
+
+
+    public double getDouble() {
+        return this.scanner.nextDouble();
+    }
+
+    // TODO: **BONUS**
+    public double getDouble(String prompt) {
+        System.out.println(prompt);
+        return this.scanner.nextDouble();
     }
 
     public double getDouble(double min, double max) {
-        System.out.printf("Enter a number between %f and %f%n", min, max);
-        double userInput = this.scanner.nextDouble();
-        if (userInput >= min && userInput <= max) {
-            return userInput;
+        System.out.printf("Give me a decimal number between %f and %f", min, max);
+        double userNumber = this.scanner.nextDouble();
+
+        if(userNumber >= min && userNumber <= max) {
+            return userNumber;
         } else {
+            System.out.println("That number is invalid.");
             return getDouble(min, max);
         }
     }
 
-    public double getDouble() {
-        System.out.println("double down let me see it");
-        double userInput = this.scanner.nextDouble();
-        return userInput;
-    }
-    public int getInt2(){
-        try {
-            return Integer.parseInt(getString());
-        } catch (NumberFormatException e) {
-            System.err.println("Invalid input, try again.");
-            return getInt2();
+    // TODO: **BONUS**
+    public double getDouble(String prompt, double min, double max) {
+        System.out.println(prompt);
+        double userNumber = this.scanner.nextDouble();
+
+        if(userNumber >= min && userNumber <= max) {
+            return userNumber;
+        } else {
+            System.out.println("That number is invalid.");
+            return getDouble(min, max);
         }
     }
-    public double getDouble2(){
-        System.out.println("double down let me see your number");
-        String userInput = scanner.nextLine();
-        try{
-            Double.valueOf(userInput);
-        }catch(NumberFormatException nfe){
-            System.out.println("That's not a number, please try again");
-            getDouble2();
-        }
-        return Double.parseDouble(userInput);
-    }
 
-    public static void main(String[] args) {
-        Input in = new Input();
-//        System.out.println(in.getString());
-        System.out.println(in.getDouble2());
 
-    }
+
+
+
+
 }
