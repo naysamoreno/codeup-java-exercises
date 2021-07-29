2b0b3944c0d950b439955ba881e789e969c6267b
Naysa Moreno <naysaymoreno@gmail.com>
2021-07-29 11:11:33 -0500

    updated circle app

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,2 +1,16 @@
-package shapes;public class CircleApp {
+
+package shapes;
+
+import util.Input;
+
+public class CircleApp {
+    public static void main(String[] args) {
+
+        Input in = new Input();
+        System.out.println("Enter the radius of a circle.");
+        Circle circle = new Circle(in.getDouble());
+        System.out.println("The area of your circle is: " + circle.getArea());
+        System.out.println("The circumference of your circle is: " + circle.getCircumference());
+
+    }
 }
